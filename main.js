1bb38f1427b7183ea0dd26cd26892fa7042a3eb0
Yo <contanct@medioalqui.mia>
2021-01-24 20:50:46 -0500

    Promer Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// alert("Elian BOBO");
+
+var parrafo = document.getElementById('parrafo');
+
+document.write("Parrafo Con document.write");
+
+parrafo.innerHTML = "Desde JS Con innerHTML";
+
+console.log("Console.log")
